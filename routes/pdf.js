c5d907e0c4f43b074ffa22858522b81c44036f6b
Fredy <fredzuna@hotmail.com>
2024-08-26 15:21:44 -0400

    create util file

--- routes/pdf.js
+++ routes/pdf.js
@@ -3,29 +3,8 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const router = express.Router();
+const { loadPDF, fillPDF } = require('../pdfUtils'); 
 
-async function loadPDF(filePath) {
-  try {
-    const existingPdfBytes = fs.readFileSync(filePath);
-    return await PDFDocument.load(existingPdfBytes);
-  } catch (error) {
-    console.error("Error loading PDF:", error);
-    throw new Error("Failed to load PDF");
-  }
-}
-
-async function fillPDF(pdfDoc, formData) {
-  const form = pdfDoc.getForm();
-
-  for (const [key, value] of Object.entries(formData)) {
-      const field = form.getField(key);
-      if (field) {
-          field.setText(value);
-      }
-  }
-
-  return pdfDoc;
-}
 
 router.get("/fill-pdf", async (req, res) => {
   try {
